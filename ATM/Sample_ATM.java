39be938e2cffc6adbb39e9a59203499397975ad4
khemraj-git <khemraj121giri@gmail.com>
2025-07-23 01:01:02 +0530

    review and update the code

--- ATM/Sample_ATM.java
+++ ATM/Sample_ATM.java
@@ -6,16 +6,23 @@ class ATM{
     int PIN;
     double balance = 0.0;
 
+
+//    creating pin
     public void pinmaker(){
         Scanner sc = new Scanner(System.in);
         System.out.println("set your 4 digit pin");
         PIN = sc.nextInt();
-
+//    condition
         if(PIN > 9999 || PIN < 0000){
             System.out.println("enter a valid pin");
         }
-
+        else{
+            System.out.println("your pin has been successfully created");
+        }
     }
+
+//      checking pin if the user has entered correct pin or not
+
 
     public void checkPIN(){
         System.out.println("enter your pin:-");
@@ -27,8 +34,41 @@ class ATM{
         else{
             System.out.println("enter a valid pin");
         }
-
     }
+
+// checking balance
+    public void checkbalance(){
+        System.out.println("balance of your account is: " + balance);
+        menu();
+    }
+
+//    withdraw money and giving condition
+    public void withdraw(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the amount you need to withdraw");
+        int amount = sc.nextInt();
+        if (amount>balance){
+            System.out.println("insufficient balance");
+        }else {
+            balance = balance - amount;
+            System.out.println("money withdraw successful");
+            System.out.println("balance of your account is: " + balance);
+
+        }
+        menu();
+    }
+
+//    deposit money there is no condition
+    public void deposit(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the amount you want to deposit");
+        int amount = sc.nextInt();
+        balance = balance + amount;
+        System.out.println("money deposit successful");
+        System.out.println("your new balance of your account is: " + balance);
+        menu();
+    }
+//    main control
     public void menu(){
         System.out.println("enter your choice:-");
         System.out.println("1. Check balance");
@@ -47,49 +87,17 @@ class ATM{
             deposit();
 
         } else if (choice==4) {
+            System.out.println("Thank you for using the ATM");
             return;
         }else {
             System.out.println("wrong choice ");
         }
-
-
-    }
-
-    public void checkbalance(){
-        System.out.println("balance of your account is: " + balance);
-        menu();
-    }
-    public void withdraw(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the amount you need to withdraw");
-        int amount = sc.nextInt();
-        if (amount>balance){
-            System.out.println("insufficient balance");
-        }else {
-            balance = balance - amount;
-            System.out.println("money withdraw successful");
-            System.out.println("balance of your account is: " + balance);
-
-        }
-        menu();
-
-    }
-    public void deposit(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the amount you want to deposit");
-        int amount = sc.nextInt();
-        balance = balance + amount;
-        System.out.println("money deposit successful");
-        System.out.println("your new balance of your account is: " + balance);
-        menu();
-
     }
 }
 
 
 public class Sample_ATM {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         ATM obj = new ATM();
         obj.pinmaker();
